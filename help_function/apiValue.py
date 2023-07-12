a6eeced8e0d98002f08dce3edc8be4fb2c3e54f1
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 12:13:34 +0530

    Update apiValue.py

--- help_function/apiValue.py
+++ help_function/apiValue.py
@@ -1,9 +1,11 @@
+from os import environ
+
 class apiValues(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    DB_NAME = os.environ.get("DB_NAME")
-    DB_URL = os.environ.get("DB_URL")
+    API_ID = int(environ.get("API_ID"))
+    API_HASH = environ.get("API_HASH")
+    BOT_TOKEN = environ.get("BOT_TOKEN")
+    DB_NAME = environ.get("DB_NAME")
+    DB_URL = environ.get("DB_URL")
     
     
                             
