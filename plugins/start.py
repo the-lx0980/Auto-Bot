612f20a799ad4903d144e2a95d469ab85a260f70
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 10:23:50 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,41 @@
+from help_function import ChatMSG
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client, message):           
+    button=InlineKeyboardMarkup([
+        [
+        InlineKeyboardButton('Help', callback_data='help')
+        ]        
+        ])
+    await message.reply_text(text=ChatMSG.START_MSG.formate(message.from_user.mention),
+                             reply_markup=button, 
+                             disable_web_page_preview=True)
+
+
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data
+    if data == "start":
+        await query.message.edit_text(
+            text=ChatMSG.START_MSG.formate(message.from_user.mention)
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton('Help', callback_data='help')
+                ]]
+                )
+            )
+    elif data == "help":
+        await query.message.edit_text(
+            text=mr.HELP_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton("Your Channel", callback_data="user_channel")
+               ],[
+               InlineKeyboardButton("Add Channel", callback_data="add_channel")
+               ],[
+               InlineKeyboardButton("🔒 Close", callback_data = "close"),
+               InlineKeyboardButton("◀️ Back", callback_data = "start")
+               ]]
+            )
+        )
