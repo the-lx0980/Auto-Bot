5ed37528db6d9b42bae24adf3bdeeff390f938e9
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 10:40:07 +0530

    Create apiValue.py

---
+++ help_function/apiValue.py
@@ -0,0 +1,9 @@
+class apiValues(object):
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_URL = os.environ.get("DB_URL")
+    
+    
+                            
